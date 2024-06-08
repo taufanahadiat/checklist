a9c69c57cdfa814bc65d4226a1bb7c55e16cee44
taufanahadiat <taufanahadiat@gmail.com>
2024-06-09 06:08:48 +0700

    1

--- enum-select-kebocoran - Copy.php
+++
@@ -1,5 +0,0 @@
-
-    <option value="A">A</option>
-    <option value="TA">TA</option>
-    <option value="RS">RS</option>
-</select>
--- pilih-view-chiller - Copy.php
+++
@@ -1,9 +0,0 @@
-          <div >
-            <label for="line">Line:</label>
-            <select class="selection-line" name="line" id="option-view-chiller">
-              <option value="" disabled selected hidden>Pilih Line</option>
-              <option value="chiller_trane_67bopet">OPP 6-7 & BOPET</option>
-              <option value="chiller_trane_45met34">OPP 4-5 & Met 3-4</option>
-              <option value="chiller_trane_coat14met12">Coat 1-4 & Met 1-2</option>
-            </select>
-          </div>
